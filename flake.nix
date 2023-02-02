fa34cc06bf9d2b534d5b2654f6361f8ad30e4e6c
wucke13 <wucke13@gmail.com>
2023-02-02 01:50:49 +0100

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+  };
+
+
+  outputs = { self, nixpkgs, flake-utils, devshell }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlay ];
+        };
+      in
+      {
+        checks = {
+          format = pkgs.runCommand "check-awk-posix"
+            { buildInputs = [ pkgs.gawk ]; } ''
+            gawk --lint -f ${./fix-gcode-posix.awk} /dev/null
+            gawk --lint=fatal -f ${./fix-gcode-posix.awk} /dev/null
+            touch $out
+          '';
+        };
+
+        devShells.default = pkgs.devshell.mkShell {
+          imports = [ "${devshell}/extra/git/hooks.nix" ];
+          name = "dev-shell";
+          packages = with pkgs; [ gawk glow super-slicer ];
+          git.hooks = {
+            enable = true;
+            pre-commit.text = ''
+              nix flake check
+            '';
+          };
+          commands = [
+            {
+              name = "help";
+              command = ''glow "$PRJ_ROOT/README.md"'';
+              help = "show the readme";
+            }
+            {
+              name = "fix-gcode";
+              command = ''
+                [ $# != 1 ] && {
+                  echo "usage: fix-gcode FILENAME"
+                  echo "will create a file with the suffix _fixed"
+                  exit
+                }
+                
+                "$PRJ_ROOT/fix-gcode-posix.awk" "$1" > "''${1%%.tsg}_fixed.tsg"
+              '';
+              help = "fix gcode, writing to a new file";
+            }
+          ];
+        };
+      }
+    );
+}
